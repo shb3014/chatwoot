31ca291bf6ccdad4d9b5d705fbb9ac5292b5281b
Gaoran Sun <33003517+shb3014@users.noreply.github.com>
2025-10-24 00:49:19 +0800

    Update openai_base_service.rb

--- lib/integrations/openai_base_service.rb
+++ lib/integrations/openai_base_service.rb
@@ -83,7 +83,7 @@ class Integrations::OpenaiBaseService
   def api_url
     endpoint = InstallationConfig.find_by(name: 'CAPTAIN_OPEN_AI_ENDPOINT')&.value.presence || 'https://api.openai.com/'
     endpoint = endpoint.chomp('/')
-    "#{endpoint}/v1/chat/completions"
+    "#{endpoint}"
   end
 
   def make_api_call(body)
