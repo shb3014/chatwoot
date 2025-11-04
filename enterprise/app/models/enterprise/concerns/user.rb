e1fbd55fad2d7711129dc8d69e9490cdc608bb8a
Gaoran Sun <33003517+shb3014@users.noreply.github.com>
2025-11-04 16:27:43 +0800

    testing3

--- enterprise/app/models/enterprise/concerns/user.rb
+++ enterprise/app/models/enterprise/concerns/user.rb
@@ -9,8 +9,9 @@ module Enterprise::Concerns::User
   end
 
   def ensure_installation_pricing_plan_quantity
-    return unless ChatwootHub.pricing_plan == 'premium'
-
-    errors.add(:base, 'User limit reached. Please purchase more licenses from super admin') if User.count >= ChatwootHub.pricing_plan_quantity
+    return
+#     return unless ChatwootHub.pricing_plan == 'premium'
+#
+#     errors.add(:base, 'User limit reached. Please purchase more licenses from super admin') if User.count >= ChatwootHub.pricing_plan_quantity
   end
 end
