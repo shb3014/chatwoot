04e92036f4614b72a39ba77650843311659c21b5
PotatoGarfield <49907655+PotatoGarfield@users.noreply.github.com>
2025-11-11 16:00:42 +0800

    test language-05

--- app/javascript/dashboard/routes/dashboard/settings/inbox/WidgetBuilder.vue
+++ app/javascript/dashboard/routes/dashboard/settings/inbox/WidgetBuilder.vue
@@ -94,6 +94,7 @@ export default {
         position: this.widgetBubblePosition,
         type: this.widgetBubbleType,
         launcherTitle: this.widgetBubbleLauncherTitle,
+        useBrowserLanguage: true,
       };
       let script = this.inbox.web_widget_script;
       return (
